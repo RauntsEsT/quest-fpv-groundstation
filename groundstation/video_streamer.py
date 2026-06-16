1e3c143866fdee09b65201036c715069c56d7e93
Rauno Arulaane <rauno.arulaane@gmail.com>
2026-06-16 10:34:36 +0300

    Video: yadif tagasi, spatiaalne-ainult hqdn3d, q:v 3

--- groundstation/video_streamer.py
+++ groundstation/video_streamer.py
@@ -161,8 +161,8 @@ class VideoStreamer:
             "-framerate", str(self.fps),
             "-thread_queue_size", "2",
             "-i", self.device,
-            "-vf", "yadif=0:-1:0,hqdn3d=1:1:2:2,scale=640:-2,format=yuvj420p",
-            "-f", "mjpeg", "-q:v", "2",
+            "-vf", "yadif=0:-1:0,hqdn3d=3:3:0:0,scale=640:-2,format=yuvj420p",
+            "-f", "mjpeg", "-q:v", "3",
             "-flush_packets", "1",
             "pipe:1",
         ]
