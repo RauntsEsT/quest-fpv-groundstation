7094d2a061a8ce905f30d6a6474be4e527adec94
Rauno Arulaane <rauno.arulaane@gmail.com>
2026-07-07 12:50:27 +0300

    main/web_server: kaivita web server varem, et UI oleks kiiremini kattesaadav

--- groundstation/web_server.py
+++ groundstation/web_server.py
@@ -402,4 +402,5 @@ async def run(vrx, tx, video_streamer, telem=None, ctrl=None,
     config = uvicorn.Config(app, host=host, port=port, log_level="warning")
     server = uvicorn.Server(config)
     log.info(f"Web UI: http://{host}:{port}")
+    await asyncio.sleep(0)
     await server.serve()
